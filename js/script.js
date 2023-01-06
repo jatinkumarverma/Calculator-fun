c35dd52ca6492ce9da391fbc87864988441fac44
JATIN KUMAR VERMA <82764118+jatinkumarverma@users.noreply.github.com>
2023-01-06 18:52:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 prompt("Please write your name:", "Name");
-prompt("Please write your partner name:", "Jatin");
+let person = prompt("Please write your partner name:", "Jatin");
 
 let loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 if (loveScore === 100) {
-  document.write("<h1>Your love score is " + loveScore + "%<br>" + " Congratulations!<br> You are made for each other!</h1>");
+  document.write("<h1>Your love score is " + loveScore + "%<br>" + " Congratulations!<br> You and " + person + " are made for each other!</h1>");
 } else if (loveScore > 40) {
-  document.write("<h1>Your love score is " + loveScore + "%<br>" + " You love each other like<br> Ranjha loves Heer!</h1>");
+  document.write("<h1>Your love score is " + loveScore + "%<br>" + " You and " + person + " love each other like<br> Ranjha loves Heer!</h1>");
 } else if (loveScore > 10) {
   document.write("<h1>Your love score is " + loveScore + "%<br>" + "RUN AWAY!!!!!</h1>");
 } else {
-  document.write("<h1>Your love score is " + loveScore + "%<br>" + " Sorry! Your partner will murder you.</h1>");
+  document.write("<h1>Your love score is " + loveScore + "%<br>" + " Sorry! " + person + " will murder you.</h1>");
 }
